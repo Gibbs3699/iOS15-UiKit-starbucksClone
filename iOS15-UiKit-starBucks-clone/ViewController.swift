30d81da7e894be9fbafd575c0c2e20479758d2e2
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-06-27 22:46:32 +0700

    Setup project

---
+++ iOS15-UiKit-starBucks-clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS15-UiKit-starBucks-clone
+//
+//  Created by TheGIZzz on 27/6/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .green
+    }
+
+
+}
+
