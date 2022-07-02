a289ea5bffdc5e4fff9bc4a9868e6decb721aa6a
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-07-02 14:57:22 +0700

    feat: Adding tableView to display content section

--- iOS15-UiKit-starBucks-clone/Home/HomeViewController.swift
+++ iOS15-UiKit-starBucks-clone/Home/HomeViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 class HomeViewController: StarBucksViewController {
 
     let homeHeaderView = HomeHeaderView()
+    let tableView = UITableView()
+    
+    let cellId = "cellId"
+    let titles = [ "Star balances", "Bonus stars", "Try these", "Welcome back", "Uplifting"
+    
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavBar()
         setTabBarImage(imageName: "house.fill", title: "Home")
+        setupTableView()
         
         style()
         layout()
@@ -30,16 +37,51 @@ extension HomeViewController {
     
     func style() {
         homeHeaderView.translatesAutoresizingMaskIntoConstraints = false
-        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
     }
     
     func layout() {
         view.addSubview(homeHeaderView)
+        view.addSubview(tableView)
         
         NSLayoutConstraint.activate([
             homeHeaderView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             homeHeaderView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            homeHeaderView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+            homeHeaderView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            tableView.topAnchor.constraint(equalTo: homeHeaderView.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
         ])
+    }
+}
+
+// MARK: - UITableView
+extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
+
+    func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+        tableView.tableFooterView = UIView()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        
+        cell.textLabel?.text = titles[indexPath.row]
+        cell.backgroundColor = .red
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titles.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 300
     }
 }
